8a53fd887df10a4d3c9327222c83420dabe6acc1
hugoresende27 <hugoresende27@gmail.com>
2022-08-08 21:09:12 +0100

    novo

--- index.php
+++ index.php
@@ -3,5 +3,17 @@
 
 echo 'docker simples';
 
+echo 'docker simples';
+
+echo 'agora para o git';
+
+echo 'docker simples';
+
+echo 'agora para o git';
+
+echo 'docker simples';
+
+echo 'agora para o git';
+
 echo 'agora para o git';
 
