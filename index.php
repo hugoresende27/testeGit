25cd96fb66ebd9cb253362e439b2cb939eb8e1b1
hugoresende27 <hugoresende27@gmail.com>
2022-08-13 11:41:07 +0100

    sabado

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php 
 	//phpinfo();
 
-echo 'docker simples';
-
-echo 'docker simples';
-
-echo 'agora para o git';
-
-echo 'docker simples';
-
-echo 'agora para o git';
-
-echo 'docker simples';
-
-echo 'agora para o git';
-
-echo 'agora para o git';
-
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
